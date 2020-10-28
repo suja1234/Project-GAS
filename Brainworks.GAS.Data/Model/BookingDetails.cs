6e577b817670c2b8c21faf843cda295af8433c1a
suja1234 <Sujatampatil13@gmail.com>
2020-10-28 19:16:03 +0530

    Updated BookingDetails Class

--- Brainworks.GAS.Data/Model/BookingDetails.cs
+++ Brainworks.GAS.Data/Model/BookingDetails.cs
@@ -1,6 +1,16 @@
-﻿namespace BrainWorks.GAS.Data
+﻿using System;
+
+namespace BrainWorks.GAS.Data
 {
 	public class BookingDetails
-	{
+	{  
+		public int Id { get; set;} 
+		public int UserId {get; set;} 
+		public int ConnectionId {get; set;} 
+		public DateTime BookedDate {get; set;} 
+		public DateTime DeliveredDate {get; set;} 
+		public string Status {get; set;} 
+		public int Money {get;set;}
+
 	}
 }
