d144f7c4cd9af8f707c60db4dede9f8e537c704e
Sagar Nakate <sagarnakate9@gmail.com>
2020-10-28 17:03:01 +0530

    Added Connection Details Module

--- Brainworks.GAS.Data/Model/ConnectionDetails.cs
+++ Brainworks.GAS.Data/Model/ConnectionDetails.cs
@@ -1,6 +1,20 @@
-﻿namespace BrainWorks.GAS.Data
+﻿using System;
+using System.Data.SqlTypes;
+using System.Text;
+namespace BrainWorks.GAS.Data
 {
 	public class ConnectionDetails
 	{
+		public int Id { get; set; }
+		public int UserId { get; set; }
+		public int ConnectionTypeId { get; set; }
+		public int ConsumerNumber { get; set; }
+		public varchar ConsumerName { get; set; }
+		public varchar DeliveryAddress { get; set; }
+		public dateTime ConnectionRequestDate { get; set; }
+		public datetime ConnectionApprovedDate { get; set; }
+		public money DepositAmount { get; set; }
+		public int Regulator { get; set; }
+		public varchar Status { get; set; }
 	}
 }
