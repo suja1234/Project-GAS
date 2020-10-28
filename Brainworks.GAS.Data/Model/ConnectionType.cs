18aff9efe37154cd4774654cd43a779e497c2086
Sagar Nakate <sagarnakate9@gmail.com>
2020-10-28 18:07:33 +0530

    Added Connection Type Module

--- Brainworks.GAS.Data/Model/ConnectionType.cs
+++ Brainworks.GAS.Data/Model/ConnectionType.cs
@@ -1,6 +1,17 @@
-﻿namespace BrainWorks.GAS.Data
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace BrainWorks.GAS.Data
 {
 	public class ConnectionType
 	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public int RefillCharge { get; set; }
+		public int NewConnectionCharge { get; set; }
+
 	}
 }
